678650ca6ba974b8197a4997d5e784230b07c64e
PM46734546 <99377246+PM46734546@users.noreply.github.com>
2023-08-02 11:37:08 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
          
-        System.out.println("~~~~~");
-        System.out.println("Red vs. Blue"); 
-        System.out.println("~~~~~");
+        //System.out.println("~~~~~");
+        //System.out.println("Red vs. Blue"); 
+        //System.out.println("~~~~~");
+
+        Main window = new Main();
+
+
     }
+
+    private Main(){
+
+        this.setVisible(true);
+
+    }
+
+
 }
